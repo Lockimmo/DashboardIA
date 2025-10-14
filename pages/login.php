e267bd8bb121607bd5ed39ce7f48c7395cdb8aba
Arthur <ar.lafaix@outlook.Fr>
2025-10-14 15:45:42 +0200

    modification orthographes 2

--- pages/login.php
+++ pages/login.php
@@ -60,7 +60,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <div class="row justify-content-center">
     <div class="col-md-6">
-        <h2 class="mb-4 text-center">Conion</h2>
+        <h2 class="mb-4 text-center">Connexion</h2>
 
         <?php if (!empty($erreur)): ?>
             <div class="alert alert-danger"><?= htmlspecialchars($erreur) ?></div>
